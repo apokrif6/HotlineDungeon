19e2892b7f90000bb09966f45525002c90c56c54
apokrif6 <j.berenchan@gmail.com>
2023-09-28 22:51:52 +0200

    Create dummy attack logic

---
+++ Source/HotlineDungeon/Public/Combat/DamageComponent.h
@@ -0,0 +1,28 @@
+﻿// HotlineDungeon
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "DamageComponent.generated.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogCombat, Log, All);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class HOTLINEDUNGEON_API UDamageComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UDamageComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};
