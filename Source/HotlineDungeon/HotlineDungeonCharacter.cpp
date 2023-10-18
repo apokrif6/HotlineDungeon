1bbe7b93d02dd7fd026ee4d6915a689846648c93
apokrif6 <j.berenchan@gmail.com>
2023-10-18 22:39:12 +0200

    Create sprint and dummy dodge

--- Source/HotlineDungeon/HotlineDungeonCharacter.cpp
+++ Source/HotlineDungeon/HotlineDungeonCharacter.cpp
@@ -26,7 +26,7 @@ AHotlineDungeonCharacter::AHotlineDungeonCharacter()
 	bUseControllerRotationYaw = false;
 	bUseControllerRotationRoll = false;
 
-	GetCharacterMovement()->bOrientRotationToMovement = true;	
+	GetCharacterMovement()->bOrientRotationToMovement = true;
 	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f);
 	GetCharacterMovement()->JumpZVelocity = 600.f;
 	GetCharacterMovement()->AirControl = 0.2f;
@@ -39,7 +39,8 @@ AHotlineDungeonCharacter::AHotlineDungeonCharacter()
 
 	// Create a follow camera
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
-	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	// Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
 	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
 
 	CombatComponent = CreateDefaultSubobject<UCombatComponent>(TEXT("CombatComponent"));
@@ -68,10 +69,12 @@ void AHotlineDungeonCharacter::SetupPlayerInputComponent(class UInputComponent* 
 	PlayerInputComponent->BindTouch(IE_Pressed, this, &AHotlineDungeonCharacter::TouchStarted);
 	PlayerInputComponent->BindTouch(IE_Released, this, &AHotlineDungeonCharacter::TouchStopped);
 
-	// VR headset functionality
-	PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &AHotlineDungeonCharacter::OnResetVR);
-
 	PlayerInputComponent->BindAction("Attack", IE_Pressed, this, &ThisClass::Attack);
+
+	PlayerInputComponent->BindAction("Sprint", IE_Pressed, this, &ThisClass::StartSprint);
+	PlayerInputComponent->BindAction("Sprint", IE_Released, this, &ThisClass::EndSprint);
+
+	PlayerInputComponent->BindAction("Dodge", IE_Pressed, this, &ThisClass::Dodge);
 }
 
 void AHotlineDungeonCharacter::Attack()
@@ -81,26 +84,33 @@ void AHotlineDungeonCharacter::Attack()
 	CombatComponent->DummyAttack();
 }
 
-
-void AHotlineDungeonCharacter::OnResetVR()
+void AHotlineDungeonCharacter::StartSprint()
 {
-	// If HotlineDungeon is added to a project via 'Add Feature' in the Unreal Editor the dependency on HeadMountedDisplay in HotlineDungeon.Build.cs is not automatically propagated
-	// and a linker error will result.
-	// You will need to either:
-	//		Add "HeadMountedDisplay" to [YourProject].Build.cs PublicDependencyModuleNames in order to build successfully (appropriate if supporting VR).
-	// or:
-	//		Comment or delete the call to ResetOrientationAndPosition below (appropriate if not supporting VR)
-	UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
+	GetCharacterMovement()->MaxWalkSpeed = SprintSpeed;
+}
+
+void AHotlineDungeonCharacter::EndSprint()
+{
+	GetCharacterMovement()->MaxWalkSpeed = DefaultSpeed;
+}
+
+void AHotlineDungeonCharacter::Dodge()
+{
+	if (GetMesh()->GetAnimInstance()->IsAnyMontagePlaying())
+		return;
+
+	float a = PlayAnimMontage(DodgeAnimMontage);
+	GEngine->AddOnScreenDebugMessage(-3, 2.f, FColor::Red, FString::SanitizeFloat(a));
 }
 
 void AHotlineDungeonCharacter::TouchStarted(ETouchIndex::Type FingerIndex, FVector Location)
 {
-		Jump();
+	Jump();
 }
 
 void AHotlineDungeonCharacter::TouchStopped(ETouchIndex::Type FingerIndex, FVector Location)
 {
-		StopJumping();
+	StopJumping();
 }
 
 void AHotlineDungeonCharacter::TurnAtRate(float Rate)
@@ -131,12 +141,12 @@ void AHotlineDungeonCharacter::MoveForward(float Value)
 
 void AHotlineDungeonCharacter::MoveRight(float Value)
 {
-	if ( (Controller != nullptr) && (Value != 0.0f) )
+	if ((Controller != nullptr) && (Value != 0.0f))
 	{
 		// find out which way is right
 		const FRotator Rotation = Controller->GetControlRotation();
 		const FRotator YawRotation(0, Rotation.Yaw, 0);
-	
+
 		// get right vector 
 		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
 		// add movement in that direction
